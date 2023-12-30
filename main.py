f811fd5801dc607c78fadf4734bddcdaf62b3a5a
Ramkumar9392 <152394275+Ramkumar9392@users.noreply.github.com>
2023-12-30 15:19:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-... # Available options
-... options = ["Rock", "Paper", "Scissors"]
-... 
-... # User input
-... user_choice = input("Choose Rock, Paper, or Scissors: ").lower()
-... 
-... # Computer selection
-... computer_choice = random.choice(options)
-... 
-... # Print computer's choice
-... print(f"Computer chose: {computer_choice}")
-... 
-... # Game logic
-... if user_choice == computer_choice:
-...         print("It's a tie!")
-... elif (user_choice == "rock"
-...  
-... and computer_choice == "scissors") or \
-...     (user_choice == "paper"
-...  
-... and computer_choice == "rock") or \
-...     (user_choice == "scissors"
-...  
-... and computer_choice == "paper"):
-...     print("You win!")
-... else:
+import random
+
+def get_user_choice():
+    user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+    while user_choice not in ['rock', 'paper', 'scissors']:
+        print("Invalid choice. Please enter rock, paper, or scissors.")
+        user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+    return user_choice
+
+def get_computer_choice():
+    return random.choice(['rock', 'paper', 'scissors'])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == 'rock' and computer_choice == 'scissors') or
+        (user_choice == 'paper' and computer_choice == 'rock') or
+        (user_choice == 'scissors' and computer_choice == 'paper')
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    print("Let's play Rock, Paper, Scissors!")
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+
+    print(f"You chose {user_choice}.")
+    print(f"Computer chose {computer_choice}.")
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+if __name__ == "__main__":
+    play_game()
+
+
